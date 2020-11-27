e53b3f75928d19a1838c906f37a9fe0f7406d79d
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-11-27 16:14:24 +0300

    Create an accordion

--- index.js
+++ index.js
@@ -1,105 +1,158 @@
-import React, { useState, useRef, forwardRef, useEffect, createElement } from 'react';
+import React, { Fragment, useState, createContext, useContext } from 'react';
 import ReactDOM from 'react-dom';
-
-// Overall, you want to be able to switch between forms.
-// 1) Turn the Login/Signup forms into controlled components
-// 2) Make just one form show up at a time
-// 3) Make the buttons toggle which component is rendered
-// 4) Forward the ref from the ToggleableForm to the components
-// 5) Make a form's first input toggled when it is active using a side effect
-
-const App = () => {
-	let data = [
-		{ name: 'Log in', component: LoginForm },
-		{ name: 'Sign up', component: SignupForm },
-	];
-	return (
-		<section>
-			<h2>Log in / Sign up</h2>
-			<ToggleableForm options={data} />
-		</section>
-	);
-};
-
-const ToggleableForm = ({ options }) => {
-	const [currentForm, setCurrentForm] = useState(0); // Change this to 1 to get the Signup form to show up
-	let focusRef = useRef(null);
-
-	return (
-		<>
-			{options.map((el, index) => {
-				return <ButtonToggle toggleForm={() => setCurrentForm(index)} index={index} key={`button${index}`} focusRef={focusRef}>{el.name}</ButtonToggle>;
-			})}
-			<FormToggle currentIndex={currentForm} focusRef={focusRef}>
-				{options.map((el, index) => {
-					return (
-						<div key={`form${index}`}>
-							{createElement(el.component, { ref: focusRef })}
-						</div>
-					);
-				})}
-			</FormToggle>
-		</>
-	);
-};
-
-const ButtonToggle = ({ children, toggleRef, toggleForm, index }) => {
-	return (
-		<button
-			onClick={() => {
-				toggleForm()
-			}}
-		>
-			{children}
-		</button>
-	);
-};
-
-const FormToggle = ({ children, currentIndex }) => {
-	if (Array.isArray(children)) {
-		return <div>{children[currentIndex]}</div>;
-		// Remember, `children` is an array when there's multiple!
-		// So, if you want to show all the forms, you just put
-		// `children`.
-		// What would you do if you just wanted to show one?
-	}
-	return null;
-};
-
-const LoginForm = forwardRef((props, ref) => {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-
-	useEffect(() => {
-		ref.current.focus();
-	}, [])
-
-	return (
-		<>
-			<input ref={ref} type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-			<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-			<button>Submit</button>
-		</>
-	);
-});
-
-const SignupForm = forwardRef((props, ref) => {
-	const [email, setEmail] = useState('');
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-
-	useEffect(() => {
-		ref.current.focus();
-	}, [])
-
-	return (
-		<>
-			<input ref={ref} type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-			<input type="text" value={username} placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-			<input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-			<button>Submit</button>
-		</>
-	);
-});
-
+function Accordion({ data, position="top", disabled=[] }) {
+  const [activeIndex, setActiveIndex] = useState(0)
+  return (
+    <div data-accordion>
+      {data.map((tab, index) => {
+        const isActive = index === activeIndex
+        const isDisabled = disabled.includes(index)
+        const title = (
+          <div
+            data-panel-title
+            className={
+              isDisabled ? 'disabled' : isActive ? 'expanded' : ''
+            }
+            onClick={() => {
+              if (!isDisabled) {
+                setActiveIndex(index)
+              }
+            }}
+          >
+            <span>{tab.label}</span>
+            <span>{tab.icon}</span>
+          </div>
+        )
+        const content = (
+          <div data-panel-content className={isActive ? 'expanded' : ''}>
+            {tab.content}
+          </div>
+        )
+        return (
+          <Fragment key={index}>
+            { position === 'bottom' ? [content, title] : [title, content] }
+          </Fragment>
+        )
+      })}
+    </div>
+  )
+}
+let AccordionContext = createContext()
+function AccordionCC({ children }) {
+  const [activeIndex, setActiveIndex] = useState(0)
+  return (
+    <div data-accordion>
+      {children.map((child, index) => {
+        return (
+          <AccordionContext.Provider
+            key={`section-${index}`}
+            value={{index, activeIndex, setActiveIndex}}
+          >
+            {child}
+          </AccordionContext.Provider>
+        )
+      })}
+    </div>
+  )
+}
+// create context on section and pass the disabled attribute on the value
+let sectionContext = createContext();
+function Section({ children, disabled = false }) {
+  return <sectionContext.Provider value={{disabled}}>
+    <div 
+      data-section
+    >
+      {children}
+    </div>
+  </sectionContext.Provider> 
+}
+function Title({ children }) {
+  let { index, activeIndex, setActiveIndex } = useContext(AccordionContext)
+  let{disabled} = useContext(sectionContext)
+  let isActive = index === activeIndex
+  return (
+    <div
+      data-panel-title
+      className={
+        disabled ? 'disabled' : isActive ? 'expanded' : ''
+      }
+      onClick={() => {
+        if (!disabled) {
+          setActiveIndex(index)
+        }
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+function Content({ children }) {
+  let { index, activeIndex } = useContext(AccordionContext)
+  let isActive = index === activeIndex
+  return (
+    <div data-panel-content className={isActive ? 'expanded' : ''}>
+      {children}
+    </div>
+  )
+}
+function App() {
+  /*const data = [
+    {
+      label: 'Paris',
+      icon: '🧀',
+      content: <Description city="paris" />,
+    },
+    {
+      label: 'Lech',
+      icon: '⛷',
+      content: <Description city="lech" />,
+    },
+    {
+      label: 'Madrid',
+      icon: '🍷',
+      content: <Description city="madrid" />,
+    },
+  ]*/
+  return (
+    <div className="App">
+      <AccordionCC>
+        <Section>
+          <Title>
+            <span>🧀</span> Paris
+          </Title>
+          <Content>
+            <Description city="paris" />
+          </Content>
+        </Section>
+        <Section disabled>
+          <Title>
+            Lech <span>⛷</span>
+          </Title>
+          <Content>
+            <Description city="lech" />
+          </Content>
+        </Section>
+        <Section>
+          <Title>
+            Madrid <span>🍷</span>
+          </Title>
+          <Content>
+            <Description city="madrid" />
+          </Content>
+        </Section>
+      </AccordionCC>
+    </div>
+  )
+}
+function Description({ city }) {
+  const data = {
+    paris:
+      "Paris is the capital and most populous city of France, with a population of 2,148,271 residents. Since the 17th century, Paris has been one of Europe's major centres of finance, diplomacy, commerce, fashion, science and the arts.",
+    lech:
+      'Lech am Arlberg is a mountain village and an exclusive ski resort in the Bludenz district in the Austrian state of Vorarlberg on the banks of the river Lech.',
+    madrid:
+      'Madrid is the capital and most populous city of Spain. As the capital city of Spain, seat of government, and residence of the Spanish monarch, Madrid is also the political, economic and cultural centre of the country.',
+  }
+  return <div>{data[city]}</div>
+}
 ReactDOM.render(<App />, document.getElementById('root'));
